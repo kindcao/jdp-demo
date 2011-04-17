e3b5ebc6868365f8c4c47287c5061e7ef4f4e4ef
kind790 <kind790@163.com>
2011-04-17 10:14:49 +0000

    new project

---
+++ src/main/java/com/action/BaseAction.java
@@ -0,0 +1,11 @@
+package com.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+
+    public static final Map<Object, Object> DATA_MAP = new HashMap<Object, Object>();
+}

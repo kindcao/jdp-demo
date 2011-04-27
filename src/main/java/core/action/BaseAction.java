d5dbe6a43ecbd8307c1aff2aac60a382f190aca4
kind790 <kind790@163.com>
2011-04-27 15:33:19 +0000

    rebuild system architecture for CRM

---
+++ src/main/java/core/action/BaseAction.java
@@ -0,0 +1,88 @@
+package core.action;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import core.json.JsonListResult;
+import core.json.JsonObjectResult;
+import core.json.JsonResult;
+import core.json.JsonValidateResult;
+
+public class BaseAction extends ActionSupport implements SessionAware {
+
+    private static final long serialVersionUID = -7367003790059602087L;
+
+    protected Map<String, Object> session;
+
+    private Integer rows = 3;
+
+    private Integer page = 1;
+
+    private String actionFlag;
+
+    private String forward;
+
+    protected void responseJsonData(JsonResult jr) throws Exception {
+        HttpServletResponse response = ServletActionContext.getResponse();
+        response.setContentType("text/json;charset=UTF-8");
+        response.setHeader("Pragma", "no-cache");
+        response.addHeader("Cache-Control", "must-revalidate");
+        response.addHeader("Cache-Control", "no-cache");
+        response.addHeader("Cache-Control", "no-store");
+        response.setDateHeader("Expires", 0);
+        response.getWriter().write(JSONObject.fromObject(jr).toString());
+        response.getWriter().flush();
+        response.getWriter().close();
+    }
+
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+
+    public Map<String, Object> getSession() {
+        return session;
+    }
+
+    public Integer getRows() {
+        return rows;
+    }
+
+    public void setRows(Integer rows) {
+        this.rows = rows;
+    }
+
+    public Integer getPage() {
+        return page;
+    }
+
+    public void setPage(Integer page) {
+        this.page = page;
+    }
+
+    public String getActionFlag() {
+        return actionFlag;
+    }
+
+    public void setActionFlag(String actionFlag) {
+        this.actionFlag = actionFlag;
+    }
+
+    public String getForward() {
+        return forward;
+    }
+
+    public void setForward(String forward) {
+        this.forward = forward;
+    }
+
+}
---
+++ src/main/java/core/listener/SessionListener.java
@@ -0,0 +1,40 @@
+package core.listener;
+
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import core.common.Constants;
+import crm.sysuser.model.SysUser;
+
+/**
+ * 
+ * @author Kind Cao
+ * @version 1.0 <br>
+ *          Apr 16, 2011 9:20:51 PM
+ */
+public class SessionListener implements HttpSessionListener {
+
+    protected Log log = LogFactory.getLog(SessionListener.class);
+
+    @Override
+    public void sessionCreated(HttpSessionEvent event) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void sessionDestroyed(HttpSessionEvent event) {
+        HttpSession session = event.getSession();
+        SysUser sysUser = (SysUser) session
+                .getAttribute(Constants.CURR_SYS_USER_SESSION_KEY);
+        if (sysUser != null) {
+            Constants.SYS_USER_MAP.remove(sysUser.getUsername());
+            session.removeAttribute(Constants.CURR_SYS_USER_SESSION_KEY);
+            log.info("user[" + sysUser.getUsername() + "] session destroyed");
+        }
+    }
+}

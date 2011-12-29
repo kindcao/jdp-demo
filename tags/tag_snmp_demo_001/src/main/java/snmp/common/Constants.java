d3d35b0a5085b91eb5499c89fa6148dbd0df4796
kind790 <kind790@163.com>
2011-12-29 03:19:53 +0000

    snmp demo

---
+++ tags/tag_snmp_demo_001/src/main/java/snmp/common/Constants.java
@@ -0,0 +1,33 @@
+package snmp.common;
+
+/**
+ * 
+ * @author Kind Cao
+ * @version 1.0 <br>
+ *          Dec 27, 2011 11:14:25 AM
+ */
+public class Constants {
+
+    public static String IFNUMBER = "1.3.6.1.2.1.2.1";
+
+    public static String IFINDEX = "1.3.6.1.2.1.2.2.1.1.";
+
+    public static String IFDESCR = "1.3.6.1.2.1.2.2.1.2.";
+
+    public static String IFTYPE = "1.3.6.1.2.1.2.2.1.3.";
+
+    public static String IFINOCTETS = "1.3.6.1.2.1.2.2.1.10.";
+
+    public static String IFOUTOCTETS = "1.3.6.1.2.1.2.2.1.16.";
+
+    public static final String UNDERLINE = "_";
+
+    public static final String DOT = ".";
+
+    public static final String CFG_FILE_PATH = "./conf/snmp_cfg.properties";
+
+    public static final String LOG_FILE_PATH = "./conf/log4j.properties";
+
+    public static final int CHAET_NUM = 2;
+
+}
